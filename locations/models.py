93c5763eea9ce42aeedf35960530f24876ce01b7
John Holstine <flyboy1565@gmail.com>
2017-06-23 18:30:58 +0000

    Fixed ModelField Choices

--- locations/models.py
+++ locations/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # third party
 from localflavor.us.models import USStateField, PhoneNumberField
+from localflavor.us.us_states import STATE_CHOICES
 from auditlog.registry import auditlog
 
 # my helpers
@@ -17,14 +18,14 @@ class Store(models.Model):
     latitude = models.DecimalField(max_digits=11, decimal_places=8)
     address = models.CharField(max_length=60, blank=True, null=True)
     city = models.CharField(max_length=60, blank=True, null=True)
-    state = models.CharField(max_length=60, blank=True, null=True)
+    state = USStateField(choices = STATE_CHOICES)
     zip_code = models.CharField(max_length=5, blank=True, null=True)
     district = models.ForeignKey('District', blank=True, null=True)
     dma = models.ForeignKey('DMA', blank=True, null=True)
     dc = models.ForeignKey('DistrbutionCenter', blank=True, null=True)
-    priority = models.IntegerField()
+    priority = models.BooleanField(default=False)
     hub = models.CharField(max_length=4, blank=True, null=True)
-    is_hub = models.IntegerField()
+    is_hub = models.BooleanField(default=False)
     previous_year_sales = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     open_date = models.DateField(blank=True, null=True)
     close_date = models.DateField(blank=True, null=True)
@@ -36,7 +37,7 @@ class Store(models.Model):
     sun_close = models.TimeField(blank=True, null=True)
     timezone = models.CharField(max_length=4, blank=True, null=True)
     full_timezone = models.CharField(max_length=50, blank=True, null=True)
-    bond = models.BooleanField(default=True)
+    bond = models.BooleanField(default=False)
     store_status = models.CharField(max_length=20, choices=location_status())
     
     def __str__(self):
